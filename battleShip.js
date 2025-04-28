035b0adc6df6a200095223f6e9a24b64dbd33acc
nassai <hei.nassai@gmail.com>
2025-04-28 16:11:59 +0300

    ajout de la valeur de rétour

--- battleShip.js
+++ battleShip.js
@@ -1,8 +1,13 @@
 const damagedOrSunk = (board, attacks) => {
+
+
+
     let result = {
         sunk: 0,
         damaged: 0,
         notTouched: 0,
         points: 0
     }
+
+    return result;
 }
